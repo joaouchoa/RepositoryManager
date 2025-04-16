7c475bf41e97400cd3b055e556e3391714f3377d
joaouchoa <joaodep2@gmail.com>
2025-04-16 14:19:16 -0300

    feat: Applying improvements to Swagger documentation

--- src/ABC.RepositoryManager.API/Controllers/ReposController.cs
+++ src/ABC.RepositoryManager.API/Controllers/ReposController.cs
@@ -7,6 +7,9 @@ using ABC.RepositoryManager.Application.Features.Repositories.Queries.GetFavorit
 
 namespace ABC.RepositoryManager.API.Controllers
 {
+    /// <summary>
+    /// Controller responsible for searching and managing GitHub repositories.
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class ReposController : ControllerBase
@@ -18,28 +21,64 @@ namespace ABC.RepositoryManager.API.Controllers
             _mediator = mediator;
         }
 
+        /// <summary>
+        /// Searches for public GitHub repositories based on the provided name.
+        /// </summary>
+        /// <param name="query">Object containing the repository name, pagination, sorting, and items per page.</param>
+        /// <returns>A list of public repositories matching the provided name, with pagination and sorting.</returns>
         [HttpGet("search", Name = "GetReposByName")]
+        [ProducesResponseType(typeof(GetRepoByNameQueryResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(List<string>), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(List<string>), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(List<string>), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetReposByNameAsync([FromQuery] GetRepoByNameQuery query)
         {
             var response = await _mediator.Send(query, HttpContext.RequestAborted);
-            return response.ToActionResult(); 
+            return response.ToActionResult();
         }
 
+
+        /// <summary>
+        /// Adds a repository to the list of locally saved favorites.
+        /// </summary>
+        /// <param name="repository">The repository information to be added to favorites.</param>
+        /// <returns>The result of the favorite operation.</returns>
         [HttpPost("favorite", Name = "CreateFavoriteRepo")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(List<string>), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(List<string>), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> CreateRepoAsync([FromBody] CreateFavoriteRepoCommand repository)
         {
             var response = await _mediator.Send(repository, HttpContext.RequestAborted);
             return response.ToActionResult();
         }
 
+        /// <summary>
+        /// Removes a repository from the list of favorites.
+        /// </summary>
+        /// <param name="repository">The repository information to be removed from favorites (ID only).</param>
+        /// <returns>The result of the remove operation.</returns>
         [HttpDelete("remove-favorite", Name = "DeleteRepo")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(List<string>), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(List<string>), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(List<string>), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> DeleteRepoAsync([FromBody] DeleteFavoriteRepoCommand repository)
         {
             var response = await _mediator.Send(repository, HttpContext.RequestAborted);
             return response.ToActionResult();
         }
 
+
+        /// <summary>
+        /// Lists all favorited repositories with support for pagination and sorting.
+        /// </summary>
+        /// <param name="query">Pagination and sorting parameters.</param>
+        /// <returns>A paged list of favorited repositories.</returns>
         [HttpGet("favorite-repositories", Name = "GetfavoriteRepos")]
+        [ProducesResponseType(typeof(GetFavoriteReposQueryResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(List<string>), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(List<string>), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> GetFavoritesRepos([FromQuery] GetFavoriteReposQuery query)
         {
             var response = await _mediator.Send(query, HttpContext.RequestAborted);
--- src/ABC.RepositoryManager.API/Program.cs
+++ src/ABC.RepositoryManager.API/Program.cs
@@ -1,5 +1,6 @@
-using ABC.RepositoryManager.API.Middlewares;
+﻿using ABC.RepositoryManager.API.Middlewares;
 using ABC.RepositoryManager.API.Setup;
+using Microsoft.OpenApi.Models;
 
 namespace ABC.RepositoryManager.API
 {
@@ -12,7 +13,31 @@ namespace ABC.RepositoryManager.API
             // Add services to the container.
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "GitHub Repository Manager API",
+                    Version = "v1",
+                    Description = "API RESTful for searching and managing GitHub repositories with favorite functionality.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "João de Deus Uchôa",
+                        Url = new Uri("https://www.linkedin.com/in/joaouchoa1"),
+                        Email = "joaoucchoa@gmail.com"
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "MIT License",
+                        Url = new Uri("https://opensource.org/licenses/MIT")
+                    }
+                });
+
+                // Comentários XML
+                var xmlFile = "ABC.RepositoryManager.API.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
             builder.Services.AddApiConfig(builder.Configuration);
             builder.Services.AddHttpClient();
 
