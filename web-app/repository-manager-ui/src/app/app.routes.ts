481036712781c19d57d43aad01b45e1a8d52bec3
joaouchoa <joaodep2@gmail.com>
2025-04-16 12:58:12 -0300

    feat: add lateral sidebar menu

--- web-app/repository-manager-ui/src/app/app.routes.ts
+++ web-app/repository-manager-ui/src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { RepoListComponent } from './components/repo-list/repo-list.component';
 import { RepoFavoritesComponent } from './components/repo-favorites/repo-favorites.component';
 
 export const routes: Routes = [
-  { path: '', component: RepoListComponent },
-  { path: 'favorites', component: RepoFavoritesComponent }
+  { path: '', component: RepoListComponent },            
+  { path: 'favorites', component: RepoFavoritesComponent } 
 ];
